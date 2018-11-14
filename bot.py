e51d83126fb9156dfeee9104dbf8d82a1f297bb3
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-14 20:35:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,6 +33,36 @@ def is_admin(ctx):
 def is_mod(ctx):
     return ctx.message.author.id == "459019082943954945"
 
+@client.command(pass_context = True)
+async def play(ctx, *, url):
+    author = ctx.message.author
+    voice_channel = author.voice_channel
+    try:
+        vc = await client.join_voice_channel(voice_channel)
+        msg = await client.say("Loading...")
+        player = await vc.create_ytdl_player("ytsearch:" + url)
+        player.start()
+        await client.say("Succesfully Loaded ur song!")
+        await client.delete_message(msg)
+    except Exception as e:
+        print(e)
+        await client.say("Reconnecting")
+        for x in client.voice_clients:
+            if(x.server == ctx.message.server):
+                await x.disconnect()
+                nvc = await client.join_voice_channel(voice_channel)
+                msg = await client.say("Loading...")
+                player2 = await nvc.create_ytdl_player("ytsearch:" + url)
+                player2.start()
+
+
+@client.command(pass_context = True)
+async def stop(ctx):
+    for x in client.voice_clients:
+        if(x.server == ctx.message.server):
+            return await x.disconnect()
+
+    return await client.say("I am not playing anyting???!")
 
 
 @client.event
